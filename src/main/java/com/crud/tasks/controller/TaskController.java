37a490dc2691848414bdfe2d1e49aeffc28839bf
Piotr Misiak <p.misiak@gmail.com>
2017-09-30 17:54:26 +0200

    14.4 update tasks.controller package

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,34 +4,35 @@ import com.crud.tasks.domain.TaskDto;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/task")
+@RequestMapping("/api/v1")
 public class TaskController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    @RequestMapping(method = RequestMethod.GET, value = "/tasks")
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(final String taskId) {
+    @RequestMapping(method = RequestMethod.GET, value = "/tasks/{id}")
+    public TaskDto getTask(@PathVariable("id") final String taskId) {
         return new TaskDto((long)1, "test title", "test_content");
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    public void deleteTask(final String taskId) {
+    @RequestMapping(method = RequestMethod.DELETE, value = "/tasks/{id}")
+    public void deleteTask(@PathVariable("id") final String taskId) {
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    @RequestMapping(method = RequestMethod.PUT, value = "/tasks")
     public TaskDto updateTask(final TaskDto taskDto) {
         return new TaskDto((long)1, "Edited test title", "Test content");
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    @RequestMapping(method = RequestMethod.POST, value = "/tasks")
     public void createTask(final TaskDto taskDto) {
     }
 }
