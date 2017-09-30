d166efc7d9b597805e04db9e4157d6a1d40d052e
Piotr Misiak <p.misiak@gmail.com>
2017-09-30 17:37:35 +0200

    update TaskApplication

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TasksApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
 }
