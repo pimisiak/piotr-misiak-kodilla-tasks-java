0731456dceff1d470684fc6838c64e8fa0f20e25
Piotr Misiak <p.misiak@gmail.com>
2017-11-02 22:28:01 +0100

    add @CrossOrigin

--- src/main/java/com/crud/tasks/trello/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/trello/controller/TrelloController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/trello")
+@CrossOrigin(origins = "*")
 public class TrelloController {
     @Autowired
     private TrelloClient trelloClient;
