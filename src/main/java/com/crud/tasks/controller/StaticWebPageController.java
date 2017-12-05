4f63d059296934b68f6c83e028c00fb96d5dbaa8
Piotr Misiak <p.misiak@gmail.com>
2017-12-06 00:06:36 +0100

    24.1 add thymeleaf

---
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -0,0 +1,16 @@
+package com.crud.tasks.controller;
+
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StaticWebPageController {
+    @RequestMapping("/")
+    public String index(final Map<String, Object> model) {
+        model.put("variable", "My Thymeleaf variable");
+        model.put("one", 1);
+        model.put("two", 2);
+        return "index";
+    }
+}
