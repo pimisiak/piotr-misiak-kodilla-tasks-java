031bc571f072a3e66cf9ca82978124901b8ba73b
Piotr Misiak <p.misiak@gmail.com>
2017-12-08 17:11:03 +0100

    update mail service test

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -1,15 +1,15 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Mail;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -28,16 +28,9 @@ public class SimpleEmailServiceTest {
                 .subject("Test")
                 .message("Test message")
                 .build();
-        final SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(mail.getMailTo());
-        mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
-        if (StringUtils.isNotBlank(mail.getToCc())) {
-            mailMessage.setCc(mail.getToCc());
-        }
         // When
         simpleEmailService.send(mail);
         // Then
-        verify(javaMailSender, times(1)).send(mailMessage);
+        verify(javaMailSender, times(1)).send(any(MimeMessagePreparator.class));
     }
 }
