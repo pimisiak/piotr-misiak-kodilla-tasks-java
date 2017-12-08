b753f92dc2462038ed063844268a289e36ed8868
Piotr Misiak <p.misiak@gmail.com>
2017-12-08 14:34:23 +0100

    update mail service

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,12 +1,10 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Mail;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
@@ -23,23 +21,11 @@ public class SimpleEmailService {
     public void send(final Mail mail) {
         LOGGER.info("Starting email preparation...");
         try {
-            final SimpleMailMessage mailMessage = createMailMessage(mail);
-            javaMailSender.send(mailMessage);
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info("Email has been sent.");
         } catch (MailException e) {
             LOGGER.error("Failed to process email sending: ", e.getMessage(), e);
         }
-    }
-
-    private SimpleMailMessage createMailMessage(final Mail mail) {
-        final SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(mail.getMailTo());
-        mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
-        if (StringUtils.isNotBlank(mail.getToCc())) {
-            mailMessage.setCc(mail.getToCc());
-        }
-        return mailMessage;
     }
 
     private MimeMessagePreparator createMimeMessage(final Mail mail) {
