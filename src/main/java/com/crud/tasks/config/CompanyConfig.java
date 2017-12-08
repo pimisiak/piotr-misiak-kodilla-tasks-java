3ca48f3322c9935d1739f8a2565332c93929215a
Piotr Misiak <p.misiak@gmail.com>
2017-12-08 15:40:00 +0100

    24.2 update mail service

---
+++ src/main/java/com/crud/tasks/config/CompanyConfig.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class CompanyConfig {
+    @Value("${info.company.name}")
+    private String companyName;
+    @Value("${info.company.email")
+    private String companyEmail;
+    @Value("${info.company.phone}")
+    private String companyPhone;
+
+    @Override
+    public String toString() {
+        return String.format("%s | %s | %s", companyName, companyEmail, companyPhone);
+    }
+}
--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -12,15 +13,20 @@ public class MailCreatorService {
     @Autowired
     private AdminConfig adminConfig;
     @Autowired
+    private CompanyConfig companyConfig;
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(final String message) {
         final Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("tasks_url", "https://kodilla-tasks.herokuapp.com/");
         context.setVariable("button", "Visist website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_details", companyConfig);
+        context.setVariable("preview_message", "Trello Card created");
+        context.setVariable("goodbay_message", "Have a nice day.");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
