fa6f04b2014e438f5283c85402673598f319fed7
Piotr Misiak <p.misiak@gmail.com>
2017-12-08 17:20:23 +0100

    24.2 update mail service

--- src/main/java/com/crud/tasks/config/CompanyConfig.java
+++ src/main/java/com/crud/tasks/config/CompanyConfig.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class CompanyConfig {
     @Value("${info.company.name}")
     private String companyName;
-    @Value("${info.company.email")
+    @Value("${info.company.email}")
     private String companyEmail;
     @Value("${info.company.phone}")
     private String companyPhone;
